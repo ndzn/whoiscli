b9bde4d0af94c14e02a3b576fd26ed07a7a0069a
ndzn <nikidurzynski@gmail.com>
2022-10-04 16:29:27 +0000

    region data and ui improvements

--- go.mod
+++ go.mod
@@ -14,6 +14,9 @@ require (
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/oschwald/geoip2-golang v1.8.0 // indirect
 	github.com/oschwald/maxminddb-golang v1.10.0 // indirect
+	github.com/tidwall/gjson v1.14.3 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.1 // indirect
 	golang.org/x/net v0.0.0-20220909164309-bea034e7d591 // indirect
 	golang.org/x/sys v0.0.0-20220804214406-8e32c043e418 // indirect
 	golang.org/x/text v0.3.7 // indirect
--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
-	"net"
 	"net/http"
 	"time"
 
@@ -13,6 +12,7 @@ import (
 	"github.com/likexian/whois"
 	whoisparser "github.com/likexian/whois-parser"
 	"github.com/manifoldco/promptui"
+	"github.com/tidwall/gjson"
 )
 
 func main() {
@@ -32,17 +32,17 @@ func main() {
 		return
 	}
 
-	s := spinner.New(spinner.CharSets[9], 100*time.Millisecond)
+	s := spinner.New(spinner.CharSets[26], 100*time.Millisecond)
 	s.FinalMSG = color.GreenString("Returned Whois Data:\n")
 
 	s.Start()
 
 	whoisInfo, err := whois.Whois(result)
 	if err != nil {
-		fmt.Printf("That isnt a valid domain. Please try again")
+		fmt.Printf("That isnt a valid domain. Please try again\n")
 		return
 	} else {
-		fmt.Println("Getting whois info...")
+		fmt.Println("Getting whois info...\n")
 	}
 	parsed, err := whoisparser.Parse(whoisInfo)
 	if err == nil {
@@ -55,18 +55,28 @@ func main() {
 		// fmt.Println("Website IP", domainIP)
 		// json, _ := http.Get("https://ipinfo.io/", net.LookupIP(parsed.Domain.Domain))
 		// fmt.Println(json)
-		domainIP, _ := net.LookupIP(parsed.Domain.Domain)
-		getLocation("20.69.96.99") // testing ip for now
-		println(domainIP)
+		//domainIP, _ := net.LookupIP(parsed.Domain.Domain)
+		// fmt.Println(color.YellowString("Data from Domain's website"))
+		getLocation("8.8.8.8") // testing ip for now
 	} else {
-		fmt.Println("This isnt a registered domain")
+		fmt.Println("This isn't a registered domain. (Is the spelling correct?)")
 	}
 
 }
 
-// function to get location from given ip
-func getLocation(domain string) {
-	fmt.Println(readRequest("https://ipinfo.io/" + domain))
+// function to get location from given ip from domain
+func getLocation(ip string) {
+	s := spinner.New(spinner.CharSets[26], 100*time.Millisecond)
+	s.FinalMSG = color.GreenString("Website provider data:\n")
+	s.Start()
+	json := readRequest("https://ipinfo.io/" + ip)
+	s.Stop()
+	// fmt.Println(json)
+	// region := gjson.Get(json, "region")
+	fmt.Println("Region:", gjson.Get(json, "region"))
+	fmt.Println("City:", gjson.Get(json, "city"))
+	fmt.Println("Country:", gjson.Get(json, "country"))
+	fmt.Println("Provider:", gjson.Get(json, "org"))
 } // fix
 
 // helper function to make a request to a web page
